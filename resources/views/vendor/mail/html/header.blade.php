7e3d20dcec4c8dbe2f574c8f0d5649365fa627ca
songoku0101200 <songoku0101200@gmail.com>
2025-02-28 21:44:24 +0800

    Update Email Logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,6 +2,9 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="{{ asset('assets/aisat.png') }}" class="logo" alt="Aisat Logo">
+@else
 {{ $slot }}
 @endif
 </a>
